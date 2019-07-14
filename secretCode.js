b3513513e941b63de74dbf00d0e1e94e4ace53c7
Sarah Tam <sarahtam718@gmail.com>
2019-07-13 20:12:54 -0500

    cornify added!

--- secretCode.js
+++ secretCode.js
@@ -4,13 +4,11 @@ const secret = 'secret';
 const keysPressed = [];
 
 function hello(e) {
-  console.log(-secret.length - 1);
-  console.log(keysPressed.length);
   keysPressed.push(e.key);
   keysPressed.splice(-secret.length - 1, keysPressed.length - secret.length);
   typed.innerHTML = keysPressed.join('');
   if (keysPressed.join('').includes(secret)) {
-    console.log('DING DING');
+    cornify_add();
   }
 }
 
